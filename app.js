a747f755929aa10c635d1bc23f37c04c6a83b31a
Kyle Heldman <kheldman@salesforce.com>
2015-05-05 10:26:40 -0400

    adding homepage

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+	res.render('home', {
+		title: 'Welcome'
+	});
+});
